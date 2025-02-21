b5ebf2db04ed3190a707e95825ad832b8194ff37
richardo1o1 <richard_1@163.com>
2025-02-21 13:21:10 +0800

    fear: add zod check

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,19 @@
 import { handle } from 'hono/vercel';
 
-import configureOpenAPI from "@/utility/configure-openapi";
-import env from "@/utility/env";
-import createApp from "@/utility/create-app";
+import createApp from "./lab/create-app";
 
 const app = createApp();
 
-configureOpenAPI(app);
+app.get("/", (c) => {
+  return c.text("Hello Hono");
+});
+
+app.get("/error", (c) => {
+  c.status(422);
+  //add a custom error message to logger
+  c.var.logger.info("Wow! Log here");
+  throw new Error("This is an error");
+});
+
 
 export const GET = handle(app);
--- middlewares/pino-logger.ts
+++ middlewares/pino-logger.ts
@@ -1,13 +1,13 @@
 import { pinoLogger } from 'hono-pino';
-import { PinoPretty } from 'pino-pretty';
-import { pino } from 'pino';
+import PinoPretty from 'pino-pretty';
+import pino from 'pino';
 import env from "@/utility/env";
 
 export function pnLogger() {
   return pinoLogger({
     pino: pino({
-      level: env.LOG_LEVEL || "info"
-    }, env.NODE_ENV === "production" ? undefined:PinoPretty()),
+      level: env?.LOG_LEVEL,
+    }, env?.NODE_ENV === "production" ? undefined:PinoPretty()),
     http: {
       reqId: ()=> crypto.randomUUID(),
     }
